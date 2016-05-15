54bee4b5189522ab6887913727ce3ace8645a5e3
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-05-15 10:01:11 +0200

    alias and reference

--- org.xtext.example.mydsl.parent/org.xtext.example.mydsl.web/WebRoot/xtext-resources/generated/mode-mydsl.js
+++ org.xtext.example.mydsl.parent/org.xtext.example.mydsl.web/WebRoot/xtext-resources/generated/mode-mydsl.js
@@ -1,6 +1,6 @@
 define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
 	var HighlightRules = function() {
-		var keywords = "Hello|ref";
+		var keywords = "Hello|alias|for";
 		this.$rules = {
 			"start": [
 				{token: "comment", regex: "\\/\\/.*$"},
